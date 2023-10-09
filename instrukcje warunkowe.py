1b3d6a59d4435582f9a17ab6c577747f464b72e3
Uczeń <Ucze@ZSO1_1>
2023-10-09 10:15:44 +0200

    kokokop

--- instrukcje warunkowe.py
+++ instrukcje warunkowe.py
@@ -2,4 +2,13 @@ liczba = int(input('Podaj liczbę od 1 do 5 '))
 
 if liczba == 1:
     print("jeden")
-    print('koniec')
+elif liczba == 2:
+    print('dwa')
+elif liczba == 3:
+    print(trzy)
+elif liczba == 4:
+    print('cztery')
+elif liczba == 5:
+    print('pięć')
+
+print('koniec')
